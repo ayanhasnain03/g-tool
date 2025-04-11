1b6a8e0f95ec6282228efe44f2bfffdf5018078f
Ayan <ayanhasnain2572006@gmail.com>
2025-04-11 22:55:47 +0530

    shape select

--- src/features/editor/components/shape-sideBar.tsx
+++ src/features/editor/components/shape-sideBar.tsx
@@ -2,7 +2,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { ToolSidebarClose } from "@/features/editor/components/tool-sidebar-close";
 import { ToolSidebarHeader } from "@/features/editor/components/tool-sidebar-header";
-import { ActiveTool } from "@/features/editor/types";
+import { ActiveTool, Editor } from "@/features/editor/types";
 import { cn } from "@/lib/utils";
 import { FaCircle, FaSquare, FaSquareFull } from "react-icons/fa";
 import { FaDiamond } from "react-icons/fa6";
@@ -10,11 +10,13 @@ import { IoTriangle } from "react-icons/io5";
 import { ShapeTool } from "./shape-tool";
 
 interface ShapeSidebarProps {
+  editor: Editor | undefined;
   activeTool: ActiveTool;
   onChangeActiveTool: (tool: ActiveTool) => void;
 }
 
 export const ShapeSidebar = ({
+  editor,
   activeTool,
   onChangeActiveTool,
 }: ShapeSidebarProps) => {
@@ -35,20 +37,22 @@ export const ShapeSidebar = ({
 
       <ScrollArea>
         <div className="grid grid-cols-3 gap-4 p-4">
-          <ShapeTool onClick={() => {}} icon={FaCircle} />
-          <ShapeTool onClick={() => {}} icon={FaSquare} />
-          <ShapeTool onClick={() => {}} icon={FaSquareFull} />
-          <ShapeTool onClick={() => {}} icon={IoTriangle} />
+          <ShapeTool onClick={() => editor?.addCircle()} icon={FaCircle} />
           <ShapeTool
-            onClick={() => {}}
+            onClick={() => editor?.addSoftRectangle()}
+            icon={FaSquare}
+          />
+          <ShapeTool
+            onClick={() => editor?.addRectangle()}
+            icon={FaSquareFull}
+          />
+          <ShapeTool onClick={() => editor?.addTriangle()} icon={IoTriangle} />
+          <ShapeTool
+            onClick={() => editor?.addInverseTriangle()}
             icon={IoTriangle}
             iconClassName="rotate-180"
           />
-          <ShapeTool
-            onClick={() => {}}
-            icon={FaDiamond}
-            iconClassName="rotate-180"
-          />
+          <ShapeTool onClick={() => editor?.addDiamond()} icon={FaDiamond} />
         </div>
       </ScrollArea>
 
--- src/features/editor/hooks/use-editor.ts
+++ src/features/editor/hooks/use-editor.ts
@@ -1,7 +1,100 @@
 "use client";
+import { useAutoResize } from "@/features/editor//hooks/use-auto-resize";
+import {
+  BuildEditorProps,
+  CIRCLE_OPTIONS,
+  DIAMOND_OPTIONS,
+  Editor,
+  RECTANGLE_OPTIONS,
+  TRIANGLE_OPTIONS,
+} from "@/features/editor/types";
 import { fabric } from "fabric";
-import { useCallback, useState } from "react";
-import { useAutoResize } from "./use-auto-resize";
+import { useCallback, useMemo, useState } from "react";
+
+const buildEditor = ({ canvas }: BuildEditorProps): Editor => {
+  const getWorkspace = () => {
+    return canvas.getObjects().find((object) => object.name === "clip");
+  };
+  const center = (object: fabric.Object) => {
+    const workspace = getWorkspace();
+    const center = workspace?.getCenterPoint();
+    if (!center) return;
+    // @ts-expect-error - _centerObject is a private Fabric.js method
+    canvas._centerObject(object, center);
+  };
+
+  const addToCanvas = (object: fabric.Object) => {
+    center(object);
+    canvas.add(object);
+    canvas.setActiveObject(object);
+  };
+
+  return {
+    addCircle: () => {
+      const object = new fabric.Circle({
+        ...CIRCLE_OPTIONS,
+      });
+      addToCanvas(object);
+    },
+    addSoftRectangle: () => {
+      const object = new fabric.Rect({
+        ...RECTANGLE_OPTIONS,
+        rx: 10,
+        ry: 10,
+      });
+      addToCanvas(object);
+    },
+    addRectangle: () => {
+      const object = new fabric.Rect({
+        ...RECTANGLE_OPTIONS,
+      });
+
+      addToCanvas(object);
+    },
+    addTriangle: () => {
+      const object = new fabric.Triangle({
+        ...TRIANGLE_OPTIONS,
+      });
+
+      addToCanvas(object);
+    },
+    addInverseTriangle: () => {
+      const HEIGHT = TRIANGLE_OPTIONS.height;
+      const WIDTH = TRIANGLE_OPTIONS.width;
+
+      const object = new fabric.Polygon(
+        [
+          { x: 0, y: 0 },
+          { x: WIDTH, y: 0 },
+          { x: WIDTH / 2, y: HEIGHT },
+        ],
+        {
+          ...TRIANGLE_OPTIONS,
+        }
+      );
+
+      addToCanvas(object);
+    },
+    addDiamond: () => {
+      const HEIGHT = DIAMOND_OPTIONS.height;
+      const WIDTH = DIAMOND_OPTIONS.width;
+
+      const object = new fabric.Polygon(
+        [
+          { x: WIDTH / 2, y: 0 },
+          { x: WIDTH, y: HEIGHT / 2 },
+          { x: WIDTH / 2, y: HEIGHT },
+          { x: 0, y: HEIGHT / 2 },
+        ],
+        {
+          ...DIAMOND_OPTIONS,
+        }
+      );
+
+      addToCanvas(object);
+    },
+  };
+};
 
 export const useEditor = () => {
   const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
@@ -11,6 +104,15 @@ export const useEditor = () => {
     canvas,
     container,
   });
+  const editor = useMemo(() => {
+    if (canvas) {
+      return buildEditor({
+        canvas,
+      });
+    }
+    return undefined;
+  }, [canvas]);
+
   const init = useCallback(
     ({
       initialCanvas,
@@ -49,17 +151,9 @@ export const useEditor = () => {
 
       setCanvas(initialCanvas);
       setConatiner(initialContainer);
-
-      const test = new fabric.Rect({
-        height: 100,
-        width: 100,
-        fill: "green",
-      });
-      initialCanvas.add(test);
-      initialCanvas.centerObject(test);
     },
     []
   );
 
-  return { init };
+  return { init, editor };
 };
