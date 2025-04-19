be2dfd2159f78ac0010574c9fc4a2a7a5779497c
Ayan <ayanhasnain2572006@gmail.com>
2025-04-20 01:06:32 +0530

    hono setup

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,7 +1,14 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import user from "./user";
+
 export const runtime = "nodejs";
-const app = new Hono().basePath("/api");
-app.route("/users", user);
+
+// Initialize the Hono app and chain routes
+const app = new Hono().basePath("/api").route("/user", user);
+
+// Handle requests
 export const GET = handle(app);
+
+// Type for the app's routes
+export type AppType = typeof app;
