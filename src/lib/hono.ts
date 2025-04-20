f75ba0c0809d3dafe05d925948dca0335b9ebd0e
Ayan <ayanhasnain2572006@gmail.com>
2025-04-20 05:49:37 +0530

    img-upload

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -1,3 +1,5 @@
 import { AppType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
+export const client = hc<AppType>(
+  process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
+);
